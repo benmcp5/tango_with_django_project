a79830ae5755cb0b2fed6e50d8475acc24053d91
Ben McPartlin <2560937m@student.gla.ac.uk>
2022-01-30 18:21:56 +0000

    chap 5 done- exercises not

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<a href='/rango/about'>About</a>")
+    context_dict={'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
-    return HttpResponse("<a href='/rango/'>Index</a>")
+    return HttpResponse("<a href='/rango/'>Index</a><img src=/")
