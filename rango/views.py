70bbf9586783fd54635b630351fb3052c3a0e6c6
Ben McPartlin <2560937m@student.gla.ac.uk>
2022-01-30 18:38:15 +0000

    Chap 3 with exercises done

--- rango/views.py
+++ rango/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context_dict={'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+    #context_dict={'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+    #return render(request, 'rango/index.html', context = context_dict)
+    return HttpResponse('Rango says hey there partner!<br><a href="/rango/about/">About</a>')
 
-    return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
-    return HttpResponse("<a href='/rango/'>Index</a><img src=/")
+    return HttpResponse('Rango says here is the about page.<br><a href="/rango/">Index</a> ')
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -23,8 +23,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.index, name='about'),
+    path('', views.about, name='about'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
-    
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+]#+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
